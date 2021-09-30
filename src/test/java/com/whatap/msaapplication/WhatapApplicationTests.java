e2a36e96dd93c119e65ac5792fe53e4e81fab0c8
SEOLMIN KWON <stat_kwon@naver.com>
2021-09-30 23:17:56 +0900

    initial commit

---
+++ src/test/java/com/whatap/msaapplication/WhatapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whatap.msaapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
