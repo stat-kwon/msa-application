98144197cc57b793c505b8e0ce2581f55fe49470
SEOLMIN KWON <stat_kwon@naver.com>
2021-10-01 05:15:41 +0900

    feat: product Entity 생성

--- src/main/java/com/whatap/msaapplication/WhatapApplication.java
+++ src/main/java/com/whatap/msaapplication/WhatapApplication.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class WhatapApplication {
 
---
+++ src/main/java/com/whatap/msaapplication/web/domain/product/Product.java
@@ -0,0 +1,27 @@
+package com.whatap.msaapplication.web.domain.product;
+
+import com.whatap.msaapplication.web.domain.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Product extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int stockQuantity;
+}
